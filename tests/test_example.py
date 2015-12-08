8c965cf2ba80fe8b316bb1c697fbd63ce75b1962
Manu Phatak <bionikspoon@gmail.com>
2015-12-07 20:58:48 -0600

    manually call '_cleanup'

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 # coding=utf-8
+from pytest import fixture
+
 from sample_module.example import redis_cache
 
 
-def test_redislite_can_set():
-    r = redis_cache()
+@fixture
+def r(request):
+    """:type request: _pytest.python.FixtureRequest"""
+    handle = redis_cache()
+    request.addfinalizer(handle._cleanup)
+    return handle
+
+
+def test_redislite_can_set(r):
     assert r.set('foo', 'bar') is True
     assert r.get('foo') == b'bar'
